72f2f8740c5558dbc470a437c1914d0f5def2e78
Pedro Ivo Zabeu <pedroivozabeu@MacBook-Air-de-Pedro.local>
2026-01-23 15:21:04 -0300

    Remove logo-showcase route

--- src/app/logo-showcase/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from "@/components/brand/Logo";
-
-export default function LogoShowcase() {
-    return (
-        <div className="min-h-screen bg-white flex items-center justify-center">
-            <Logo className="scale-125" />
-        </div>
-    );
-}
